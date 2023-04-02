16db694c39a96fca09aca3e6a004f987566ca1e8
MestreWilll <willianichigo@hotmail.com>
2023-04-02 07:34:34 -0300

    Primeiro teste_1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+function alterarBackground() {
+  document.body.style.backgroundColor = '#a73c3c';
+  document.body.style.backgroundImage = 'url(./Imagens/El Pistoleiro criando vida.gif)';
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundPosition = 'center';
+}
+
+function centralizarTitulo() {
+  const titulo = document.getElementById('titulo');
+  titulo.style.position = 'absolute';
+  titulo.style.top = '50%';
+  titulo.style.left = '50%';
+  titulo.style.transform = 'translate(-50%, -50%)';
+  titulo.style.textAlign = 'center';
+}
